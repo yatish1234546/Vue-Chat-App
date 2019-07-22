9fd99e3781bb2c5d6c922d52184a7a9497ad78cc
Yatish Katagar <katagaryatish@gmail.com>
2019-07-22 22:03:51 +0530

    Updated Firebase Details

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,13 +1,7 @@
 import firebase from 'firebase';
 import firestore from 'firebase/firestore'
 var firebaseConfig = {
-    apiKey: "AIzaSyBp0Z7Nqjo6IHdVfPGPyUClXaZgIcf2OZY",
-    authDomain: "chat-app-f4b74.firebaseapp.com",
-    databaseURL: "https://chat-app-f4b74.firebaseio.com",
-    projectId: "chat-app-f4b74",
-    storageBucket: "chat-app-f4b74.appspot.com",
-    messagingSenderId: "464057488272",
-    appId: "1:464057488272:web:6746341c965a0d67"
+
 };
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
