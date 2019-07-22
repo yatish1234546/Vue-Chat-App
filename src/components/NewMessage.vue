7c25043807bc9187cf3b2493ab23c9fb27c899b1
Yatish Katagar <katagaryatish@gmail.com>
2019-07-22 21:59:57 +0530

    Vue Chat App

---
+++ src/components/NewMessage.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="new-message">
+        <form @submit.prevent="addMessage">
+            <label for="new-message">New Message(Enter to send)</label>
+            <input type="text" name="new-message" v-model="newMessage">
+            <p v-if="feedback" class="red-text">{{feedback}}</p>
+        </form>
+    </div>
+</template>
+<script>
+import db from "@/firebase/init";
+export default {
+  name: "NewMessage",
+  props: ["name"],
+  data() {
+    return {
+      newMessage: null,
+      feedback: null
+    };
+  },
+  methods: {
+    addMessage() {
+      if (this.newMessage) {
+        db
+          .collection("messages")
+          .add({
+            content: this.newMessage,
+            name: this.name,
+            timestamp: Date.now()
+          })
+          .catch(e => console.log(e));
+        this.newMessage = null;
+        this.feedback = null;
+      } else {
+        this.feedback = "You must enter the message";
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
